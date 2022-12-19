50651a0911829b5303a2c03c03cf15fd4b425f07
hurmat <hurmatkhalid1822@gmail.com>
2022-12-19 19:07:23 +0530

    Banner 50%

---
+++ components/Banner.tsx
@@ -0,0 +1,32 @@
+const Banner = () => {
+	return (
+		<div className="h-screen w-full flex justify-center items-center">
+			<div className="flex flex-col md:flex-row justify-between w-full items-center px-4 md:px-20">
+				<div className="flex flex-col w-1/2 items-start space-y-4">
+					<h1 className="font-bold text-5xl uppercase tracking-widest text-gray-300">
+						Guiding Towards Growth
+					</h1>
+
+					<p className="text-base leading-5 tracking-wider">
+						21Consequences is a non profit organisation, founded by a bunch of
+						young people from Kashmir, to guide students, especially high school
+						kids academically. As an organisation, we help students in narrowing
+						down streams for career preference, provide prep materials, books,
+						tips and strategies regarding multiple competitive examinations held
+						worldwide and spread awareness about different opportunities.
+					</p>
+					<div className="flex items-center space-x-4">
+						<button className="px-4 py-2 flex items-center justify-center rounded-full border-2 border-[#3b1e7f] hover:bg-[#3b1e7f] transition-all duration-200 ease-in">
+							Read More
+						</button>
+						<button className="px-4 py-2 rounded-full flex items-center justify-center border-2 border-[#3b1e7f] hover:bg-[#3b1e7f] transition-all duration-200 ease-in">
+							Watch Video
+						</button>
+					</div>
+				</div>
+				<div>Logo</div>
+			</div>
+		</div>
+	);
+};
+export default Banner; //this is like flushing(?)
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,22 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import Banner from '../components/Banner';
 import Header from '../components/Header';
 
 const Home: NextPage = () => {
 	return (
-		<div className="min-h-screen bg-black text-white">
+		<div className="min-h-screen w-full bg-black text-white">
 			<Head>
 				<title>21Consequences</title>
 				<link rel="icon" href="/Logo.png" />
 			</Head>
 
 			<Header />
+
+			<main className="max-w-7xl mx-auto">
+				<Banner />
+			</main>
 		</div>
 	);
 };
