6d0705e6bd99e139293dba5fdd1c36cf35100d05
hurmat <hurmatkhalid1822@gmail.com>
2022-12-21 00:00:12 +0530

    fixed bugs

--- components/Discord.tsx
+++ components/Discord.tsx
@@ -1,71 +1,71 @@
-import React from "react";
-import Image from "next/image";
-import { motion } from "framer-motion";
+import React from 'react';
+import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 type Props = {};
 
 const Discord = (props: Props) => {
-  return (
-    <div className="h-screen w-full snap-start flex justify-center items-center">
-      <div className="flex flex-col md:flex-row justify-between w-full items-center px-4 md:px-20">
-        <motion.div
-          initial={{
-            y: 200,
-            opacity: 0,
-          }}
-          whileInView={{
-            y: 0,
-            opacity: 1,
-          }}
-          transition={{
-            duration: 1,
-          }}
-          viewport={{ once: true }}
-          className="flex flex-col w-1/2 items-start justify-start space-y-4"
-        >
-          <h1 className="font-bold text-4xl uppercase tracking-widest text-gray-300">
-            Join Our Student Community on Discord
-          </h1>
-          <p className="text-base leading-5 tracking-wider text-gray-400">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
-            sint tempora labore tempore vitae voluptates odit nihil quam vel ab
-            culpa mollitia magnam delectus voluptas, quo aut, reprehenderit
-            illum? Omnis. Lorem ipsum dolor sit amet consectetur adipisicing
-            elit. Eius excepturi libero fugit laborum nostrum cum officia
-            similique, laboriosam corporis consequatur incidunt. Adipisci
-            asperiores accusamus earum beatae nobis laboriosam eius qui? Lorem
-            ipsum dolor sit, amet consectetur adipisicing elit. Officia
-            repudiandae magnam, rem maxime neque error! Officia, ipsa beatae
-            molestiae vitae accusamus illo cumque reprehenderit reiciendis harum
-            adipisci sint necessitatibus rem.
-          </p>
-        </motion.div>
+	return (
+		<div
+			id="discord"
+			className="min-h-fit w-full snap-center flex justify-center md:pb-20 items-center">
+			<div className="flex flex-col md:flex-row justify-between w-full items-center px-4 md:px-20">
+				<motion.div
+					initial={{
+						y: 200,
+						opacity: 0,
+					}}
+					whileInView={{
+						y: 0,
+						opacity: 1,
+					}}
+					transition={{
+						duration: 1,
+					}}
+					viewport={{ once: true }}
+					className="flex flex-col w-1/2 items-start justify-start space-y-4">
+					<h1 className="font-bold text-4xl uppercase tracking-widest text-gray-300">
+						Join Our Student Community on Discord
+					</h1>
+					<p className="text-base leading-5 tracking-wider text-gray-400">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+						sint tempora labore tempore vitae voluptates odit nihil quam vel ab
+						culpa mollitia magnam delectus voluptas, quo aut, reprehenderit
+						illum? Omnis. Lorem ipsum dolor sit amet consectetur adipisicing
+						elit. Eius excepturi libero fugit laborum nostrum cum officia
+						similique, laboriosam corporis consequatur incidunt. Adipisci
+						asperiores accusamus earum beatae nobis laboriosam eius qui? Lorem
+						ipsum dolor sit, amet consectetur adipisicing elit. Officia
+						repudiandae magnam, rem maxime neque error! Officia, ipsa beatae
+						molestiae vitae accusamus illo cumque reprehenderit reiciendis harum
+						adipisci sint necessitatibus rem.
+					</p>
+				</motion.div>
 
-        <motion.div
-          initial={{
-            x: 200,
-            opacity: 0,
-          }}
-          whileInView={{
-            x: 0,
-            opacity: 1,
-          }}
-          transition={{
-            duration: 1.5,
-          }}
-          viewport={{ once: true }}
-        >
-          <Image
-            src="/ss.png"
-            alt="discord"
-            height={600}
-            width={600}
-            className="rounded-xl"
-          />
-        </motion.div>
-      </div>
-    </div>
-  );
+				<motion.div
+					initial={{
+						x: 200,
+						opacity: 0,
+					}}
+					whileInView={{
+						x: 0,
+						opacity: 1,
+					}}
+					transition={{
+						duration: 1.5,
+					}}
+					viewport={{ once: true }}>
+					<Image
+						src="/ss.png"
+						alt="discord"
+						height={600}
+						width={600}
+						className="rounded-xl"
+					/>
+				</motion.div>
+			</div>
+		</div>
+	);
 };
 
 export default Discord;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,14 @@ import About from '../components/About';
 import Banner from '../components/Banner';
 import Discord from '../components/Discord';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const desc =
 	'21Consequences is a non profit organisation, founded by a bunch of young people from Kashmir, to guide students, especially high school kids academically. As an organisation, we help students in narrowing down streams for career preference, provide prep materials, books, tips and strategies regarding multiple competitive examinations held worldwide and spread awareness about different opportunities.';
 
 const Home: NextPage = () => {
 	return (
-		<div className="h-full overflow-y-scroll scrollbar-none w-full bg-black text-white">
+		<div className="min-h-screen w-full overflow-y-scroll scrollbar-none bg-black text-white">
 			<Head>
 				<title>21Consequences</title>
 				<link rel="icon" href="/Logo.png" />
@@ -20,7 +21,7 @@ const Home: NextPage = () => {
 
 			<Header />
 
-			<div className="max-w-[90rem] scroll-smooth snap-y h-screen overflow-y-scroll scrollbar-none snap-mandatory mx-auto">
+			<div className="max-w-[90rem] mx-auto">
 				<Banner />
 				<About />
 				<Discord />
