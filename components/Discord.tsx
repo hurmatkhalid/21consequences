cee76ea5c184c08c3ebefa6331ce91e345ae3065
Sameem <ssssameembaba@gmail.com>
2022-12-20 19:59:09 +0530

    Description add

--- components/Discord.tsx
+++ components/Discord.tsx
@@ -1,41 +1,41 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
 type Props = {};
 
 const Discord = (props: Props) => {
-	return (
-		<div className="h-screen w-full snap-start flex justify-center items-center">
-			<div className="flex flex-col md:flex-row justify-between w-full items-center px-4 md:px-20">
-				<div className="flex flex-col w-1/2 items-start justify-start space-y-4">
-					<h1 className="font-bold text-4xl uppercase tracking-widest text-gray-300">
-						Join Our Student Community on Discord
-					</h1>
-					<p className="text-base leading-5 tracking-wider text-gray-400">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
-						sint tempora labore tempore vitae voluptates odit nihil quam vel ab
-						culpa mollitia magnam delectus voluptas, quo aut, reprehenderit
-						illum? Omnis. Lorem ipsum dolor sit amet consectetur adipisicing
-						elit. Eius excepturi libero fugit laborum nostrum cum officia
-						similique, laboriosam corporis consequatur incidunt. Adipisci
-						asperiores accusamus earum beatae nobis laboriosam eius qui? Lorem
-						ipsum dolor sit, amet consectetur adipisicing elit. Officia
-						repudiandae magnam, rem maxime neque error! Officia, ipsa beatae
-						molestiae vitae accusamus illo cumque reprehenderit reiciendis harum
-						adipisci sint necessitatibus rem.
-					</p>
-				</div>
+  return (
+    <div className="h-screen w-full snap-start flex justify-center items-center">
+      <div className="flex flex-col md:flex-row justify-between w-full items-center px-4 md:px-20">
+        <div className="flex flex-col w-1/2 items-start justify-start space-y-4">
+          <h1 className="font-bold text-4xl uppercase tracking-widest text-gray-300">
+            Join Our Student Community on Discord
+          </h1>
+          <p className="text-base leading-5 tracking-wider text-gray-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+            sint tempora labore tempore vitae voluptates odit nihil quam vel ab
+            culpa mollitia magnam delectus voluptas, quo aut, reprehenderit
+            illum? Omnis. Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Eius excepturi libero fugit laborum nostrum cum officia
+            similique, laboriosam corporis consequatur incidunt. Adipisci
+            asperiores accusamus earum beatae nobis laboriosam eius qui? Lorem
+            ipsum dolor sit, amet consectetur adipisicing elit. Officia
+            repudiandae magnam, rem maxime neque error! Officia, ipsa beatae
+            molestiae vitae accusamus illo cumque reprehenderit reiciendis harum
+            adipisci sint necessitatibus rem.
+          </p>
+        </div>
 
-				<Image
-					src="/ss.png"
-					alt="discord"
-					height={600}
-					width={600}
-					className="rounded-xl"
-				/>
-			</div>
-		</div>
-	);
+        <Image
+          src="/ss.png"
+          alt="discord"
+          height={600}
+          width={600}
+          className="rounded-xl"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Discord;
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,118 +1,117 @@
 //FOR HEADER RELATED WORK
-import Image from 'next/image';
-import { SocialIcon } from 'react-social-icons';
-import { motion } from 'framer-motion';
-import { useEffect, useState } from 'react';
+import Image from "next/image";
+import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 //for including images in nextjs we need this
 const Header = () => {
-	const [show, handleShow] = useState(false);
-	const [hoveredIcon, setHoveredIcon] = useState('');
+  const [show, handleShow] = useState(false);
+  const [hoveredIcon, setHoveredIcon] = useState("");
 
-	const transitionNavBar = () => {
-		if (window.scrollY > 100) {
-			handleShow(true);
-		} else {
-			handleShow(false);
-		}
-	};
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
 
-	useEffect(() => {
-		window.addEventListener('scroll', transitionNavBar);
-		return () => window.removeEventListener('scroll', transitionNavBar);
-	}, []);
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
 
-	const styleLink =
-		'text-gray-500 font-semibold tracking-widest cursor-pointer uppercase transitions';
+  const styleLink =
+    "text-gray-500 font-semibold tracking-widest cursor-pointer uppercase transitions";
 
-	//this is called functional component, we include functions in this
-	return (
-		//we write the html and css code here
-		<div className="fixed top-0 ">
-			<div
-				className={`px-6 py-2 h-20 flex transition-all duration-300 ease-in-out items-center w-screen justify-between ${
-					//javascript stars
-					show &&
-					'bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-60 shadow-xl'
-				}`}>
-				{/* Image */}
-				<motion.div
-					initial={{
-						x: -200,
-						opacity: 0,
-					}}
-					animate={{
-						x: 0,
-						opacity: 1,
-					}}
-					transition={{
-						duration: 1.5,
-					}}
-					className="flex">
-					<div className="flex items-center space-x-2 sm:border-r sm:border-gray-600">
-						<Image src="/Logo.png" height={40} width={40} alt="Logo" />
-						<p className="tracking-widest font-semibold pr-5 text-gray-100 uppercase">
-							21Consequences
-						</p>
-					</div>
-					<div className="sm:flex items-center pl-7 hidden space-x-4 md:space-x-10 lg:space-x-12">
-						<p className={styleLink}>Home</p>
-						<p className={styleLink}>Resources</p>
-						<p className={styleLink}>Library</p>
-						<p className={styleLink}>Team</p>
-					</div>
-				</motion.div>
-				<motion.div
-					initial={{
-						x: 200,
-						opacity: 0,
-					}}
-					animate={{
-						x: 0,
-						opacity: 1,
-					}}
-					transition={{
-						duration: 1.5,
-					}}
-					className="hidden md:block">
-					<div className="flex items-center space-x-2">
-						<SocialIcon
-							url="https://instagram.com/21consequences/"
-							target="_blank" //so that the link opens in new tab
-							onMouseEnter={() => setHoveredIcon('instagram')} //onmouseenter is an inbuilt function for anything when mouse goes on a position
-							onMouseLeave={() => setHoveredIcon('')} // this needs to be done or else the icon would stay purple
-							fgColor={hoveredIcon == 'instagram' ? '#3b1e7f' : 'gray'}
-							bgColor="transparent"
-						/>
-						<SocialIcon
-							url="https://twitter.com/21consequences/"
-							target="_blank"
-							onMouseEnter={() => setHoveredIcon('twitter')}
-							onMouseLeave={() => setHoveredIcon('')}
-							fgColor={hoveredIcon == 'twitter' ? '#3b1e7f' : 'gray'}
-							bgColor="transparent"
-						/>
-						<SocialIcon
-							url="https://www.linkedin.com/company/21consequences/"
-							target="_blank"
-							onMouseEnter={() => setHoveredIcon('linkedin')}
-							onMouseLeave={() => setHoveredIcon('')}
-							fgColor={hoveredIcon == 'linkedin' ? '#3b1e7f' : 'gray'}
-							bgColor="transparent"
-						/>
-						<SocialIcon
-							url="https://discord.gg/FSm4QqcAKd/"
-							target="_blank"
-							onMouseEnter={() => setHoveredIcon('discord')}
-							onMouseLeave={() => setHoveredIcon('')}
-							fgColor={hoveredIcon == 'discord' ? '#3b1e7f' : 'gray'}
-							bgColor="black"
-						/>
-					</div>
-				</motion.div>
-			</div>
-		</div>
-	);
+  //this is called functional component, we include functions in this
+  return (
+    //we write the html and css code here
+    <div className="fixed top-0 ">
+      <div
+        className={`px-6 py-2 h-20 flex transition-all duration-300 ease-in-out items-center w-screen justify-between bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-60 shadow-xl`}
+      >
+        {/* Image */}
+        <motion.div
+          initial={{
+            x: -200,
+            opacity: 0,
+          }}
+          animate={{
+            x: 0,
+            opacity: 1,
+          }}
+          transition={{
+            duration: 1.5,
+          }}
+          className="flex"
+        >
+          <div className="flex items-center space-x-2 sm:border-r sm:border-gray-600">
+            <Image src="/Logo.png" height={40} width={40} alt="Logo" />
+            <p className="tracking-widest font-semibold pr-5 text-gray-100 uppercase">
+              21Consequences
+            </p>
+          </div>
+          <div className="sm:flex items-center pl-7 hidden space-x-4 md:space-x-10 lg:space-x-12">
+            <p className={styleLink}>Home</p>
+            <p className={styleLink}>Resources</p>
+            <p className={styleLink}>Library</p>
+            <p className={styleLink}>Team</p>
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{
+            x: 200,
+            opacity: 0,
+          }}
+          animate={{
+            x: 0,
+            opacity: 1,
+          }}
+          transition={{
+            duration: 1.5,
+          }}
+          className="hidden md:block"
+        >
+          <div className="flex items-center space-x-2">
+            <SocialIcon
+              url="https://instagram.com/21consequences/"
+              target="_blank" //so that the link opens in new tab
+              onMouseEnter={() => setHoveredIcon("instagram")} //onmouseenter is an inbuilt function for anything when mouse goes on a position
+              onMouseLeave={() => setHoveredIcon("")} // this needs to be done or else the icon would stay purple
+              fgColor={hoveredIcon == "instagram" ? "#3b1e7f" : "gray"}
+              bgColor="transparent"
+            />
+            <SocialIcon
+              url="https://twitter.com/21consequences/"
+              target="_blank"
+              onMouseEnter={() => setHoveredIcon("twitter")}
+              onMouseLeave={() => setHoveredIcon("")}
+              fgColor={hoveredIcon == "twitter" ? "#3b1e7f" : "gray"}
+              bgColor="transparent"
+            />
+            <SocialIcon
+              url="https://www.linkedin.com/company/21consequences/"
+              target="_blank"
+              onMouseEnter={() => setHoveredIcon("linkedin")}
+              onMouseLeave={() => setHoveredIcon("")}
+              fgColor={hoveredIcon == "linkedin" ? "#3b1e7f" : "gray"}
+              bgColor="transparent"
+            />
+            <SocialIcon
+              url="https://discord.gg/FSm4QqcAKd/"
+              target="_blank"
+              onMouseEnter={() => setHoveredIcon("discord")}
+              onMouseLeave={() => setHoveredIcon("")}
+              fgColor={hoveredIcon == "discord" ? "#3b1e7f" : "gray"}
+              bgColor="black"
+            />
+          </div>
+        </motion.div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
